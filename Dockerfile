bebf6bf9db33be1d66e333a98dbf94d3b5ac74ba
Rafał Pitoń <kontakt@rpiton.com>
2018-04-15 00:26:21 +0200

    Some extra things

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.6
+ENV PYTHONUNBUFFERED 1
+
+RUN \
+  apt-get -y update && \
+  apt-get install -y gettext && \
+  apt-get clean
+
+ADD requirements.txt /app/
+RUN pip install -r /app/requirements.txt
+
+ADD . /app
+WORKDIR /app
+
+EXPOSE 8000
+ENV PORT 8000
+
+CMD ["uwsgi", "/app/misagodocker/wsgi/uwsgi.ini"]
---
+++ wait_for_postgres.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# This utility script pauses docker's execution until we are sure that PostgreSQL has started
+export PGPASSWORD=$POSTGRES_PASSWORD
+RETRIES=10
+
+until psql -h postgres -U $POSTGRES_USER -d $POSTGRES_DB -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for PostgreSQL to start, $((RETRIES--)) remaining attempts..."
+  sleep 3
+done
