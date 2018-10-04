26ef2e13ad3e7e220f1107bbe323afa200ff0f88
rafalp <kontakt@rpiton.com>
2018-10-04 21:23:36 +0200

    Basic email setup change support

--- wizard/setup.py
+++ wizard/setup.py
@@ -5,5 +5,8 @@ from postgres import run_postgres_wizard
 WIZARDS_ANDS_FILES = ((run_misago_wizard, misago), (run_postgres_wizard, postgres))
 
 if __name__ == "__main__":
-    for run_wizard, env_file in WIZARDS_ANDS_FILES:
-        run_wizard(env_file)
+    try:
+        for run_wizard, env_file in WIZARDS_ANDS_FILES:
+            run_wizard(env_file)
+    except KeyboardInterrupt:
+        print()
