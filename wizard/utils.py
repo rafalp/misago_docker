c4e10a7832de0a16a15a706e190f75c57850f3b6
rafalp <kontakt@rpiton.com>
2018-10-07 18:53:59 +0200

    Add appctl reload command

--- wizard/utils.py
+++ wizard/utils.py
@@ -54,5 +54,6 @@ def serialize_bool(value):
 def print_setup_changed_message():
     print(
         "Changes saved. "
-        'Remember to run "./appctl restart" to load new configuration.'
+        'Remember to run "./appctl reload" to rebuild and restart '
+        "container to use new configuration."
     )
