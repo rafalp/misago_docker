60543645214c3650986f44da5de1de6dab7577c2
rafalp <kontakt@rpiton.com>
2019-10-05 22:57:56 +0200

    Add plugin configuration support

--- misago/misagodocker/settings.py
+++ misago/misagodocker/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+
+from misago import load_plugin_list_if_exists
+
 from .utils import strtobool, strtolist
 
 
@@ -202,7 +205,11 @@ CSRF_COOKIE_SECURE = True
 
 CSRF_FAILURE_VIEW = 'misago.core.errorpages.csrf_failure'
 
-INSTALLED_APPS = [
+PLUGINS_LIST_PATH = os.path.join(BASE_DIR, "plugins.txt")
+
+INSTALLED_PLUGINS = load_plugin_list_if_exists(PLUGINS_LIST_PATH) or []
+
+INSTALLED_APPS = INSTALLED_PLUGINS + [
     # Misago overrides for Django core feature
     'misago',
     'misago.users',
@@ -245,6 +252,7 @@ INSTALLED_APPS = [
     "misago.faker",
     "misago.menus",
     "misago.sso",
+    "misago.plugins",
 ]
 
 INTERNAL_IPS = [
